d47e27918c67254c87ca6a4088e52c0c0dbe92e6
t-rindade <tr.ndade3@gmail.com>
2023-08-08 21:41:11 -0300

    react

---
+++ vsconnect/src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+// import Home from "./pages/Home";
+import Servicos from "./pages/Servicos";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <Home /> */}
+    <Servicos />
+  </React.StrictMode>
+);
